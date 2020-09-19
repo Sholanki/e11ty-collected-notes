87d4f9416fafc1c91d2437f9e8e109207872f0ed
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-09-19 18:17:25 +0530

    Add files via upload

--- _includes/layout.11ty.js
+++ _includes/layout.11ty.js
@@ -20,13 +20,31 @@ module.exports = ({ content }) => /*html*/`
                 ${content}
             </main>
         </body>
-		<form
-  action="https://buttondown.email/api/emails/embed-subscribe/reddy2go"
-  method="post"
-  target="popupwindow"
-  onsubmit="window.open('https://buttondown.email/reddy2go', 'popupwindow')"
-  class="embeddable-buttondown-form"
->
+		 <form
+      action="https://buttondown.email/api/emails/embed-subscribe/reddy2go"
+      method="post"
+      target="popupwindow"
+      onSubmit={submitAction}
+    >
+      <div className="grid grid-cols-3 grid-rows-1 space-x-1 gap-1">
+        <label className="max-w-xs self-end text-right" htmlFor="bd-email">
+          subscribe to the newsletter
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="bd-email"
+          className="border border-black"
+        />
+        <input type="hidden" value="1" name="embed" />
+
+        <input
+          type="submit"
+          value="Subscribe"
+          className="text-black rounded-sm bg-gray-300 font-medium"
+        />
+      </div>
+    </form>
   <label for="bd-email">Enter your email</label>
   <input type="email" name="email" id="bd-email"></input>
   <input type="hidden" value="1" name="embed"></input>
