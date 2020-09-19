eb8a50fa01b6f23e8b7ba3bcc0e45ce7e2650ec0
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-09-19 19:11:56 +0530

    Add files via upload

--- _includes/layout.11ty.js
+++ _includes/layout.11ty.js
@@ -28,7 +28,7 @@ module.exports = ({ content }) => /*html*/`
       onSubmit={submitAction}
     >
       <div class="grid grid-cols-3 grid-rows-1 space-x-1 gap-1" style="padding-bottom: 50px;">
-        <label class="max-w-xs self-end text-right" htmlFor="bd-email">
+        <label class="text2 text-right" htmlFor="bd-email">
           subscribe to the newsletter
         </label>
         <input
@@ -42,7 +42,7 @@ module.exports = ({ content }) => /*html*/`
         <input
           type="submit"
           value="Subscribe"
-          class="text-black rounded-sm button font-medium"
+          class="button"
         />
       </div>
     </form>
